054be3eafc1d6003baa71bc0a238481722d71ca3
Vikash Gupta <vikashgupta@Vikashs-MacBook-Pro.local>
2026-07-01 23:34:44 +0530

    updated CORS

--- src/main/java/com/car/dekho/config/CorsConfig.java
+++ src/main/java/com/car/dekho/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("https://my-first-angular-project-*.vercel.app","http://localhost:4200")
+                        .allowedOriginPatterns("https://car-dekho-frontend-*.vercel.app","http://localhost:4200")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
